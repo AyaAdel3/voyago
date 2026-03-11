6b43b7d1ba107fe2b0e3b8c613de0c8e7b09a162
AyaAdel3 <aya.adel.voyago@gmail.com>
2026-03-11 23:02:19 +0200

    update navbar

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -8,6 +8,7 @@ import { ForgotPassword } from '../../../core/Auth/forgot-password/forgot-passwo
 import { EnterCode } from '../../../core/Auth/enter-code/enter-code';
 import { ResetPassword } from '../../../core/Auth/reset-password/reset-password';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -15,11 +16,11 @@ import { Router } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-
 export class Navbar implements OnInit {
   isDarkMode = false;
 
-constructor(public modal: AuthModalService, public router: Router) {}
+  constructor(public modal: AuthModalService, public router: Router) {}
+
   ngOnInit(): void {
     const savedMode = localStorage.getItem('darkMode');
     if (savedMode === 'true') {
@@ -41,4 +42,25 @@ constructor(public modal: AuthModalService, public router: Router) {}
       document.body.classList.remove('dark-mode');
     }
   }
+
+  isHotelsActive(): boolean {
+    return this.router.url.startsWith('/hotels');
+  }
+
+  isRestaurantsActive(): boolean {
+    return this.router.url.startsWith('/Restaurants') || this.router.url.startsWith('/restaurant');
+  }
+
+  isAttractionsActive(): boolean {
+    return this.router.url.startsWith('/Attractions') || this.router.url.startsWith('/tourist-attraction');
+  }
+
+  isTourGuideActive(): boolean {
+    return this.router.url.startsWith('/tour-guide');
+  }
+
+ isBudgetActive(): boolean {
+  const url = decodeURIComponent(this.router.url);
+  return url.startsWith('/Budget Planning') || url.startsWith('/budget-planning');
+}
 }
