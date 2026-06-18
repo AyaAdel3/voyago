eae7a5cc5065060e2db9eec7f1f053ca03626d71
AyaAdel3 <aya.adel.voyago@gmail.com>
2026-06-18 14:07:21 +0300

    Fix API URL to HTTPS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://voyagoo.runasp.net'
+  apiUrl: 'https://voyagoo.runasp.net'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://voyagoo.runasp.net'
+  apiUrl: 'https://voyagoo.runasp.net'
 };
